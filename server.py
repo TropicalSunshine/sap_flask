27f182bf1b77ccae89e0ce79e1cc0082a2173e9c
Jason C. Liu <32473162+TropicalSunshine@users.noreply.github.com>
2020-10-29 01:06:59 -0700

    add routes

--- server.py
+++ server.py
@@ -1,15 +1,46 @@
 from flask import Flask
+import json
+
 app = Flask(__name__)
 
 @app.route("/<int:number>", methods=["GET"])
-def range():
-
+def range(number):
+    result = []
+    for n in range(1, number + 1):
+        result.append(n)
+    return result
 
 @app.route("/<int:number>/odd", methods=["GET"])
-def odd_range():
+def odd_range(number):
+    result = []
+    for n in range(1, number + 1):
+        if(n % 2 == 1):
+            result.append(n)
+    return result
 
-@app.route("/<int:number>/odd", methods=["GET"])
-def even_range():
+@app.route("/<int:number>/even", methods=["GET"])
+def even_range(number):
+    result = []
+    for n in range(1, number + 1):
+        if(n % 2 == 0):
+            result.append(n) 
+    return result
 
 
-@app.route("")
+@app.route("/<int:number>/prime", methods=["GET"])
+def prime_range(number):
+    result = [];
+    s = [];
+    
+    for n in range(1, number + 1):
+        s.append(True)
+    
+    from n in range(2, number + 1)
+        if(s[n] == true):
+            result.append(n);
+            
+            for b in range(2, number + 1):
+                s[n * b] = False
+    
+    return result;
+
